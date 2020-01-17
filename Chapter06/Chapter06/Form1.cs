e5b9eb1d9a2eb283f9bc4662c35a402c46a87f72
Andressa Yuge <yuriyuge04@gmail.com>
2020-01-16 19:59:26 -0500

    Chapter06

---
+++ Chapter06/Chapter06/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+           
+            
+                bool flag;
+                int val;
+                string output = "Number is even";
+
+                //Convert from text yo number
+                flag = int.TryParse(txtNumber.Text, out val);
+                if (flag == false)
+                {
+                    MessageBox.Show("Not a number. Re-enter.");
+                    txtNumber.Clear();
+                    txtNumber.Focus();
+                    return;
+                }
+                  // See if odd or even
+            if (val % 2 == 1)
+            {
+
+                output = "Number is odd";
+
+            }//Show result
+             lblOutput.Text = output;
+        }     
+
+    }
+}
